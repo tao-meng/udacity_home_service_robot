84d747c2c5dfa36b7c94a6bb2fe5253c7f7679a1
Tao <tao_nk@163.com>
2019-08-07 16:17:32 +0800

    use myoffice world file

--- src/scripts/test_slam.sh
+++ src/scripts/test_slam.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/map/my.world " &
+xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/map/myoffice.world " &
 sleep 5
 xterm -e " roslaunch turtlebot_gazebo gmapping_demo.launch " &
 sleep 5
